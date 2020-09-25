ecd03b6c9bd521e78fe01cf077052aca39eac4d5
Tan Yi Jie <Jayyyy123@users.noreply.github.com>
2020-09-25 19:14:57 +0800

    First commit

---
+++ src/Chat.js
@@ -0,0 +1,50 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
+import React from "react";
+import "./Chat.css";
+
+function Chat({ messages }) {
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input
+            placeholder="Type a message"
+            type="text"
+          />
+          <button 
+          type="submit">
+            Send a message
+          </button>
+        </form>
+        <MicIcon />
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBG2l23ghziDyNEaU0zhGgTCermdxcMvC8",
+  authDomain: "whatsapp-mern-eb6c9.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-eb6c9.firebaseio.com",
+  projectId: "whatsapp-mern-eb6c9",
+  storageBucket: "whatsapp-mern-eb6c9.appspot.com",
+  messagingSenderId: "629059509898",
+  appId: "1:629059509898:web:02fb7039b4902ec0db5773"
+};
